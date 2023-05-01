66742c3cf5d141c1f77db277a4d793326e3b4c12
sabinagva <93854417+sabinagva@users.noreply.github.com>
2023-05-01 15:55:42 -0500

    Initial commit

---
+++ client.js
@@ -0,0 +1,47 @@
+/*
+You’ve been given an index.html file and this starter js file.
+
+0. Source this file into the index.html file. You should see a console log when this is done correctly.
+  -- There is no JS to write for this step.
+*/
+
+console.log('Step 0 complete! client.js loaded!');
+            
+// Write your code here!
+
+// 1. Start with the number 29 and set that equal to a variable named ‘highScore’;
+
+// 2. Make a new empty array called myScrabbleTiles.
+
+// 3. Put these 6 tile objects into myScrabbleTiles:
+/*
+  { tile: 'N', score: 1 }
+  { tile: 'K', score: 5 }
+  { tile: 'Z', score: 10 }
+  { tile: 'X', score: 8 }
+  { tile: 'D', score: 2 }
+  { tile: 'A', score: 1 }
+*/
+
+// 4. Remove the last tile from myScrabbleTiles.
+
+
+// 5. Add the following new tile to myScrabbleTiles:
+/*
+  { tile: 'F', score : 4 }
+*/
+
+// 6. Complete this function. It needs to be given an array of tile objects. The function will use a for-loop and return the sum of all the scores in the given array.
+function sumTiles(anArray) {
+
+}
+
+
+// 7. Use the function above to get the total score for myScrabbleTiles and assign the value to a variable called "myScore".
+
+
+// 8. Check whether or not your score is higher than the highScore.
+// 8-1 If your score is higher, change highScore to the new high score.
+
+
+// DONE!
